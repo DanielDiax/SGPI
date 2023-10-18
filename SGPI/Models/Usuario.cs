e88415cb1de08133160ec2ea17689e6a0501556f
Daniel Diaz Valencia <jose.diaz773@pascualbravo.edu.co>
2023-10-17 21:12:15 -0500

    Agregar archivos de proyecto.

---
+++ SGPI/Models/Usuario.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SGPI.Models;
+
+public partial class Usuario
+{
+    public int IdUsuario { get; set; }
+
+    public string Nombre { get; set; } = null!;
+
+    public string PrimerApellido { get; set; } = null!;
+
+    public string? SegundoApellido { get; set; }
+
+    public string Email { get; set; } = null!;
+
+    public string Password { get; set; } = null!;
+
+    public int NumeroDoc { get; set; }
+
+    public bool Activo { get; set; }
+
+    public int IdDoc { get; set; }
+
+    public int IdGenero { get; set; }
+
+    public int IdPrograma { get; set; }
+
+    public int IdRol { get; set; }
+
+    public virtual ICollection<Homologacion> Homologacions { get; set; } = new List<Homologacion>();
+
+    public virtual TipoDocumento IdDocNavigation { get; set; } = null!;
+
+    public virtual Genero IdGeneroNavigation { get; set; } = null!;
+
+    public virtual Programa IdProgramaNavigation { get; set; } = null!;
+
+    public virtual Rol IdRolNavigation { get; set; } = null!;
+
+    public virtual ICollection<Pago> Pagos { get; set; } = new List<Pago>();
+}
