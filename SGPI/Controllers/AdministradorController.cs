f6602346918484636069fce8e5b004b93bfd01bd
Daniel Diaz Valencia <jose.diaz773@pascualbravo.edu.co>
2023-10-17 21:42:31 -0500

    Se añaden nuevos imputs al formulario de creacion de usuarios

--- SGPI/Controllers/AdministradorController.cs
+++ SGPI/Controllers/AdministradorController.cs
@@ -1,11 +1,29 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 
+using SGPI.Models;
+
 namespace SGPI.Controllers
 {
     public class AdministradorController : Controller
     {
+        private SgpiContext context;
+
+        public AdministradorController(SgpiContext sgpiContext) 
+        {
+            context = sgpiContext;
+        }
+
         public IActionResult Index()
         {
+            return View();
+        }
+
+        public IActionResult CrearUsuario()
+        {
+            ViewBag.genero = context.Generos.ToList();
+
+            ViewBag.rol = context.Rols.ToList();
+
             return View();
         }
     }
