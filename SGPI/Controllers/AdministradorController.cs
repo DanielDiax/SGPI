fbdf3f0e2330d1c87a5c88d6dbbfd3969f570687
Daniel Diaz Valencia <jose.diaz773@pascualbravo.edu.co>
2023-10-26 12:55:06 -0500

    Progreso en aplicación -- Guardar Ok

--- SGPI/Controllers/AdministradorController.cs
+++ SGPI/Controllers/AdministradorController.cs
@@ -18,11 +18,68 @@ namespace SGPI.Controllers
             return View();
         }
 
+        /// <summary>
+        /// Consulta los Drop down para la vista de creación de usuarios
+        /// </summary>
+        /// <returns></returns>
         public IActionResult CrearUsuario()
         {
             ViewBag.genero = context.Generos.ToList();
 
             ViewBag.rol = context.Rols.ToList();
+
+            ViewBag.tipoDocumento = context.TipoDocumentos.ToList();
+
+            return View();
+        }
+
+        /// <summary>
+        /// Guarda el nuevo usuario
+        /// </summary>
+        /// <param name="usuario"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public IActionResult CrearUsuario(Usuario usuario)
+        {
+            context.Add(usuario);
+            context.SaveChanges();
+
+            ViewBag.genero = context.Generos.ToList();
+
+            ViewBag.rol = context.Rols.ToList();
+
+            ViewBag.tipoDocumento = context.TipoDocumentos.ToList();
+
+            return View();
+
+        }
+
+        /// <summary>
+        /// Consulta los Drop down para la vista de creación de usuarios
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult ModificarUsuario()
+        {
+            ViewBag.genero = context.Generos.ToList();
+
+            ViewBag.rol = context.Rols.ToList();
+
+            ViewBag.tipoDocumento = context.TipoDocumentos.ToList();
+
+            return View();
+        }
+
+        /// <summary>
+        /// Guarda el nuevo usuario
+        /// </summary>
+        /// <param name="usuario"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public IActionResult BuscarUsuario(Usuario user)
+        {
+            var us = context.Usuarios
+                .Where(u => u.NumeroDoc
+                .Contains(user.NumeroDoc));
 
             return View();
         }
